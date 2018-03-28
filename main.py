11bdb249e4cbcab4ae86288ca98bdd323a082e6d
J.P. Brichta <jpbrichta@users.noreply.github.com>
2018-03-28 05:59:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Assignment 1
+# ICS3U
+# <your name>
+# March 28, 2018
+
+###### uncomment this when you are ready to work on it
+#def CtoF ():
+#
+
+###### uncomment this when you are ready to work on it
+#def FtoC ():
+#
+
+temperature = int(input('Enter your temperature in Celsius: '))
+print(temperature)
